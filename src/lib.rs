336d6ba6260ace259f9ff2f4277568cdcc689d7f
Mearkatz <drquinsey@gmail.com>
2024-03-09 18:01:56 -0500

    improve trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
 use inquire::{InquireError, MultiSelect, Select};
-use std::str::FromStr;
-use strum::VariantNames;
+use std::{fmt::Display, str::FromStr, vec::Vec};
+use strum::VariantArray;
 
-trait SelectVariant: VariantNames + FromStr {
+pub trait SelectVariant: VariantArray + Display + FromStr + Clone {
     /// Selects a single variant from this Enum.
+    /// # Errors
+    ///
+    /// Will return `Err` if no variant is selected or there is a problem getting user input from the terminal.
+
     fn select_one_variant(message: &str) -> Result<Self, InquireError> {
-        Select::new(message, Self::VARIANTS.to_vec())
-            .prompt()
-            .map(|x| unsafe { Self::from_str(x).unwrap_unchecked() })
+        Select::new(message, Self::VARIANTS.to_vec()).prompt()
     }
 
     /// Selects multiple variants from this Enum.
+    /// # Errors
+    ///
+    /// Will return `Err` if no variants are selected or there is a problem getting user input from the terminal.
     fn select_multiple_variants(message: &str) -> Result<Box<[Self]>, InquireError> {
         MultiSelect::new(message, Self::VARIANTS.to_vec())
             .prompt()
-            .map(|x| {
-                x.into_iter()
-                    .map(|s| unsafe { Self::from_str(s).unwrap_unchecked() })
-                    .collect()
-            })
+            .map(Vec::into_boxed_slice)
     }
 }
