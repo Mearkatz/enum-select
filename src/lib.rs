d7c72ea7b846087fa6e2c9ae8314cc9450a46817
Mearkatz <drquinsey@gmail.com>
2024-03-11 00:14:48 -0400

    complete rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,12 @@
 use inquire::{InquireError, MultiSelect, Select};
-use std::{fmt::Display, str::FromStr, vec::Vec};
+use std::{fmt::Display, vec::Vec};
 use strum::VariantArray;
 
-pub trait SelectVariant: VariantArray + Display + FromStr + Clone {
-    /// Selects a single variant from this Enum.
-    /// # Errors
-    ///
-    /// Will return `Err` if no variant is selected or there is a problem getting user input from the terminal.
-
-    fn select_one_variant(message: &str) -> Result<Self, InquireError> {
-        Select::new(message, Self::VARIANTS.to_vec()).prompt()
+trait Selectable: VariantArray + Display + Clone {
+    fn select_one(message: &str) -> Select<'_, Self> {
+        Select::new(message, Self::VARIANTS.to_vec())
     }
-
-    /// Selects multiple variants from this Enum.
-    /// # Errors
-    ///
-    /// Will return `Err` if no variants are selected or there is a problem getting user input from the terminal.
-    fn select_multiple_variants(message: &str) -> Result<Box<[Self]>, InquireError> {
+    fn select_multiple(message: &str) -> MultiSelect<'_, Self> {
         MultiSelect::new(message, Self::VARIANTS.to_vec())
-            .prompt()
-            .map(Vec::into_boxed_slice)
     }
 }
